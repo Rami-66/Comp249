8ed576bef977b65745df070d8f3a14cb02085d60
Rami-66 <149551030+Rami-66@users.noreply.github.com>
2024-08-26 13:30:39 -0400

    Add files via upload

---
+++ Assignment1/src/Package1/WheeledTransportation.java
@@ -0,0 +1,7 @@
+package Package1;
+
+public class WheeledTransportation {
+	private int numOfWheels;
+	private double maxSpeed;
+
+}
---
+++ Assignment1/src/Package2/Train.java
@@ -0,0 +1,13 @@
+package Package2;
+
+import Wheel.WheeledTransportation;
+
+public class Train extends WheeledTransportation {
+
+	private int  numofVehicles;
+	private String nameOfTheStartingStation;
+	private String nameOfTheDestinationStation;
+	
+	
+	
+}
